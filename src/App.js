db79f67a5692ec5c6db35e0019d63b7ba2c5e310
sutthikiat1 <42085251+sutthikiat1@users.noreply.github.com>
2020-04-29 18:44:21 +0700

    infinite Scroll Gallery

--- src/App.js
+++ src/App.js
@@ -1,46 +1,144 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Card, CardContent } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import Loadmore from './components/Loadmore';
+import axios from 'axios';
+const keyUnsplas = 's73nTc9WvbURJYxPJ9jjL9BDGXb4pD705NQlZi3poUM';
 
-const Div = styled.div`
-  margin : 0 auto;
-  width : 50%;
-  
-  div {
-    width : 100%;
-    height : 80px;
-    text-align : center;
-    margin : 3px;
-    color : white;
+const GlobalStyle = createGlobalStyle`
+  body {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    background: #060c21;
   }
 `
 
-const posts = new Array(50).fill('Lorm ipsm dolor sit amet consectetur.').map((data, index) => `${index + 1}: ${data}`); //Dummp Data
-const firstDisplay = posts.slice(0, 10); // Slice Data เริ่มต้น = 10
+const Div = styled.div`
+  display : flex;
+  flex-flow:  row wrap;
+  justify-content : center;
+
+  @media(max-width : 768px) {
+      width : 100%;
+  }
+`
+const Box = styled.div`
+  margin-top : 10px;
+  margin-left : 10px;
+  position : relative;
+  width : 200px;
+  height : 300px;
+  background : #060c21;
+
+  &:before {
+    content : '';
+    position : absolute;
+    top : -2px;
+    left : -2px;
+    right: -2px;
+    bottom : -2px;
+    z-index : -1 ;
+  }
+
+  &:after {
+    content : '';
+    position : absolute;
+    top : -2px;
+    left : -2px;
+    right: -2px;
+    bottom : -2px;
+    z-index : -2 ;
+    filter : blur(40px);
+  }
+
+  &:before,:after {
+    background : linear-gradient(235deg,#89ff00,#060c21,#00bcd4);
+  }
+
+  @media(max-width : 768px) {
+    width : 80px;
+    height : 120px;
+  }
+`
+const PriveImage = styled.div`
+  img {
+    object-fit : cover;
+    width : 100%;
+    height : 300px;
+    filter: grayscale(50%) brightness(80%);
+    transition : 0.5s;
+  }
+  
+  &:hover {
+    filter: saturate(203%) brightness(125%);
+  }
+
+  @media(max-width : 768px) {
+    img {
+      width : 80px;
+      height : 120px;
+    }
+  }
+`
+const Title = styled.div`
+  color : white;
+  text-align : center;
+  font-size : 20px;
+  font-weight : bold;
+  padding : 30px;
+  text-shadow : 5px 10px 16px #09DF94;
+`
 
 function App() {
-  const [displayPost, setDisplayPost] = useState(firstDisplay);
+  const [displayPost, setDisplayPost] = useState([]);
+  const [page, setPage] = useState(1)
+
+  const getPhotos = () => {
+    axios.get(`https://api.unsplash.com/photos/?client_id=${keyUnsplas}&page=${page}`).then((res) => {
+      let { data } = res;
+      setDisplayPost([...displayPost, ...data])
+      setPage(page + 1);
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  useEffect(() => {
+    const callApi = async () => {
+      await getPhotos();
+    }
+    callApi();
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <React.Fragment>
-      {displayPost.map((post) => {
-        return (
-          <Div key={post}>
-            <Card style={{ backgroundImage: 'linear-gradient(to right top, #27508e, #3b63a2, #4e76b6, #608acb, #739ee0)' }}>
-              <CardContent>{post}</CardContent>
-            </Card>
-          </Div>
-        )
-      })}
+      <GlobalStyle />
+      <Title>
+        <span role="img" aria-label="photo">Example React With Infinite Scroll Gallery 📷</span>
+      </Title>
+      <Div>
+        {displayPost.map((display, index) => {
+          const { alt_description, urls: { regular } } = display
+          return (
+            <React.Fragment key={index}>
+              <Box>
+                <React.Fragment>
+                  <PriveImage>
+                    <img key={index} src={regular} alt={alt_description} />
+                  </PriveImage>
+                </React.Fragment>
+              </Box>
+            </React.Fragment>
+          )
+        })}
 
+      </Div>
       <Loadmore
-        display={displayPost}
-        setDisplay={setDisplayPost}
-        data={posts}
+        loadData={getPhotos}
       />
     </React.Fragment>
-  );
+  )
 }
+
 
 export default App;
--- src/components/Loadmore.js
+++ src/components/Loadmore.js
@@ -3,26 +3,30 @@ import styled from 'styled-components';
 
 const LoadingText = styled.div`
   margin : 0 auto;
+  margin-top : 20px;
   text-align : center;
-  font-size : 25px;
+  font-size : 20px;
   font-weight : bold;
+
+  color : white !important;
 `
 
 function Loadmore(props) {
-    const {display , setDisplay , data } = props;
+    const { loadData } = props;
     const [observedEl, setObservedEl] = useState(null);
 
     const loadMore = () => { //3.Set LoadingMore || ทำการ slice data เพิ่มเข้าไปใน display
         setTimeout(() => { // หน่วงเวลา
-            setDisplay([...display, ...data.slice(display.length, data.length > display.length + 10 ? display.length + 10 : data.length)]) //เช็ค ถ้า Data มากกว่าข้อมูลที่แสดง ให้เพิ่ม Data + 10 ใน display แต่ถ้าไม่ใช่ ให้เอา data ทั้งหมดมา
+            loadData();
         }, 500)
     }
 
     const observer = new IntersectionObserver((items) => { //2.Check Observer Targer || ถ้าหน้าจอ focus element ที่ต้องการแล้วให้ Load data
         if (items[0].isIntersecting) {
-            loadMore();
+            loadMore()
         }
     }, { threshold: 1 })
+
 
     useEffect(() => {
         if (observedEl) observer.observe(observedEl); //1.ถ้ามี element ให้ทำการติดตาม หรือ observe element นั้น ๆ
@@ -32,13 +36,12 @@ function Loadmore(props) {
         }
     }, [observedEl, observer])
 
+
     return (
         <div>
-            {data.length > display.length &&
-                <LoadingText>
-                    <p ref={setObservedEl}>Loading more...</p> 
-                </LoadingText>
-            }
+            <LoadingText>
+                <p ref={setObservedEl}>Loading more...</p>
+            </LoadingText>
         </div>
     )
 }
